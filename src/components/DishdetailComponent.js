d9ba13b4d394b5230253ecf4e9e459b3f4211714
Ammar Kazem <kazem.ammar49@gmail.com>
2020-10-29 08:41:38 +0100

    New components

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Dish extends Component() {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedDish: null
+        }
+    }
+
+    onDish
+}
